d6b250d2c4d78bcc342f09e0411b03f733684f63
Yonas Bekele <yonasbek4@gmail.com>
2025-04-08 11:00:28 +0300

    other linting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,8 +16,9 @@ const eslintConfig = [
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-empty-object-type": "off",
-      "@typescript-eslint/no-empty-interface": "off"
-
+      "@typescript-eslint/no-empty-interface": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@react/no-unescaped-entities": "off"
     },
   },
 ];
