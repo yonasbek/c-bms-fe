0d9537a5bf08f787c17e965b093c2ae0a895ab0c
Yonas Bekele <yonasbek4@gmail.com>
2025-05-16 09:35:19 +0300

    badge try fix

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -27,10 +27,12 @@ export interface BadgeProps
   extends React.HTMLAttributes<HTMLDivElement>,
     VariantProps<typeof badgeVariants> {}
 
-function Badge({ className, variant, ...props }: BadgeProps) {
-  return (
-    <div className={cn(badgeVariants({ variant }), className)} {...props} />
-  )
-}
+const Badge = React.forwardRef<HTMLDivElement, BadgeProps>(
+  ({ className, variant, ...props }, ref) => {
+    return (
+      <div className={cn(badgeVariants({ variant }), className)} {...props} ref={ref} />
+    )
+  }
+)
 
 export { Badge, badgeVariants }
